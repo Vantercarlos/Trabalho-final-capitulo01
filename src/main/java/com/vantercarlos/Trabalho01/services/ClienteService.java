c7f0627851b12e8975c22bdcd129b52391319f7d
vantercarlos <vantercarlos.caetano@cs.inf.br>
2021-05-23 10:02:26 -0400

    Created service, resource, GET ID ok

--- src/main/java/com/vantercarlos/Trabalho01/services/ClienteService.java
+++ src/main/java/com/vantercarlos/Trabalho01/services/ClienteService.java
@@ -3,10 +3,14 @@ package com.vantercarlos.Trabalho01.services;
 import com.vantercarlos.Trabalho01.dto.ClientDTO;
 import com.vantercarlos.Trabalho01.entities.Client;
 import com.vantercarlos.Trabalho01.repositories.ClientRepository;
+import com.vantercarlos.Trabalho01.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -14,8 +18,16 @@ public class ClienteService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
         Page<Client> list = repository.findAll(pageRequest);
         return list.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
     }
 }
